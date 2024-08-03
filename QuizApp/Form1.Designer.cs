eba0224c96109d8efd041141c92b4ddc0bec09ee
ChiyunaZen <uni202406_11@telework.to>
2024-08-03 10:08:25 +0900

    フォームデザインの基本配置

--- QuizApp/Form1.Designer.cs
+++ QuizApp/Form1.Designer.cs
@@ -28,13 +28,116 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.questionLabel = new System.Windows.Forms.Label();
+            this.option1RadioButton = new System.Windows.Forms.RadioButton();
+            this.option2RadioButton = new System.Windows.Forms.RadioButton();
+            this.option3RadioButton = new System.Windows.Forms.RadioButton();
+            this.option4RadioButton = new System.Windows.Forms.RadioButton();
+            this.submitButton = new System.Windows.Forms.Button();
+            this.resultLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // questionLabel
+            // 
+            this.questionLabel.AutoSize = true;
+            this.questionLabel.Location = new System.Drawing.Point(134, 19);
+            this.questionLabel.Name = "questionLabel";
+            this.questionLabel.Size = new System.Drawing.Size(43, 15);
+            this.questionLabel.TabIndex = 0;
+            this.questionLabel.Text = "label1";
+            this.questionLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // option1RadioButton
+            // 
+            this.option1RadioButton.AutoSize = true;
+            this.option1RadioButton.Location = new System.Drawing.Point(98, 67);
+            this.option1RadioButton.Name = "option1RadioButton";
+            this.option1RadioButton.Size = new System.Drawing.Size(110, 19);
+            this.option1RadioButton.TabIndex = 1;
+            this.option1RadioButton.TabStop = true;
+            this.option1RadioButton.Text = "radioButton1";
+            this.option1RadioButton.UseVisualStyleBackColor = true;
+            // 
+            // option2RadioButton
+            // 
+            this.option2RadioButton.AutoSize = true;
+            this.option2RadioButton.Location = new System.Drawing.Point(98, 105);
+            this.option2RadioButton.Name = "option2RadioButton";
+            this.option2RadioButton.Size = new System.Drawing.Size(110, 19);
+            this.option2RadioButton.TabIndex = 2;
+            this.option2RadioButton.TabStop = true;
+            this.option2RadioButton.Text = "radioButton1";
+            this.option2RadioButton.UseVisualStyleBackColor = true;
+            // 
+            // option3RadioButton
+            // 
+            this.option3RadioButton.AutoSize = true;
+            this.option3RadioButton.Location = new System.Drawing.Point(98, 145);
+            this.option3RadioButton.Name = "option3RadioButton";
+            this.option3RadioButton.Size = new System.Drawing.Size(110, 19);
+            this.option3RadioButton.TabIndex = 3;
+            this.option3RadioButton.TabStop = true;
+            this.option3RadioButton.Text = "radioButton1";
+            this.option3RadioButton.UseVisualStyleBackColor = true;
+            // 
+            // option4RadioButton
+            // 
+            this.option4RadioButton.AutoSize = true;
+            this.option4RadioButton.Location = new System.Drawing.Point(98, 184);
+            this.option4RadioButton.Name = "option4RadioButton";
+            this.option4RadioButton.Size = new System.Drawing.Size(110, 19);
+            this.option4RadioButton.TabIndex = 4;
+            this.option4RadioButton.TabStop = true;
+            this.option4RadioButton.Text = "radioButton1";
+            this.option4RadioButton.UseVisualStyleBackColor = true;
+            // 
+            // submitButton
+            // 
+            this.submitButton.Location = new System.Drawing.Point(98, 235);
+            this.submitButton.Name = "submitButton";
+            this.submitButton.Size = new System.Drawing.Size(110, 37);
+            this.submitButton.TabIndex = 5;
+            this.submitButton.Text = "確認する";
+            this.submitButton.UseVisualStyleBackColor = true;
+            // 
+            // resultLabel
+            // 
+            this.resultLabel.AutoSize = true;
+            this.resultLabel.Location = new System.Drawing.Point(134, 292);
+            this.resultLabel.Name = "resultLabel";
+            this.resultLabel.Size = new System.Drawing.Size(22, 15);
+            this.resultLabel.TabIndex = 6;
+            this.resultLabel.Text = "仮";
+            this.resultLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(313, 354);
+            this.Controls.Add(this.resultLabel);
+            this.Controls.Add(this.submitButton);
+            this.Controls.Add(this.option4RadioButton);
+            this.Controls.Add(this.option3RadioButton);
+            this.Controls.Add(this.option2RadioButton);
+            this.Controls.Add(this.option1RadioButton);
+            this.Controls.Add(this.questionLabel);
+            this.Name = "Form1";
+            this.Text = "クイズアプリ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label questionLabel;
+        private System.Windows.Forms.RadioButton option1RadioButton;
+        private System.Windows.Forms.RadioButton option2RadioButton;
+        private System.Windows.Forms.RadioButton option3RadioButton;
+        private System.Windows.Forms.RadioButton option4RadioButton;
+        private System.Windows.Forms.Button submitButton;
+        private System.Windows.Forms.Label resultLabel;
     }
 }
 
