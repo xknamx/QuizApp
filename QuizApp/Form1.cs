529d7d6f395fb05f714d321b3d874a0e19bb4163
ChiyunaZen <uni202406_11@telework.to>
2024-08-03 10:29:07 +0900

    LoadQuestionメソッドの作成
    Question型のリストを作成してクイズを格納するメソッド

--- QuizApp/Form1.cs
+++ QuizApp/Form1.cs
@@ -20,6 +20,31 @@ namespace QuizApp
             InitializeComponent();
         }
 
+        private void LoadQuestion()
+        //クイズの内容を読み込むメソッド
+        {
+            questions = new List<Question>
+            //Question型リストquestionを新しく作り
+            {
+                //Questionクラスで設定したQuestion型の要素を代入して追加
+                new Question
+                {
+                    Text = "日本の首都はどこですか？",
+                    Options = new string[] { "大阪", "東京", "京都", "福岡" },
+                    CorrectOption = 1
+                },
+
+                new Question
+                {
+                    Text="次のうち、２＋２の答えはどれですか？",
+                    Options =new string[]{"3","4","5","6"},
+                    CorrectOption=1
+                }
+
+                //ここに質問を追加
+            };
+        }
+
         private void SubmitButton_Click(object sender, EventArgs e)
         {
 
