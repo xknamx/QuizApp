3bf711d15869d79a930de99d51bfed6057f278e4
ChiyunaZen <uni202406_11@telework.to>
2024-08-03 11:14:03 +0900

    Questionクラスのコンストラクタを定義

--- QuizApp/Form1.cs
+++ QuizApp/Form1.cs
@@ -120,5 +120,12 @@ namespace QuizApp
         public string Text { get; set; }
         public string[] Options { get; set; }
         public int CorrectOption { get; set; }
+
+        public Question(string text, string[] options, int correctOption)
+        {
+            Text = text;
+            Options = options;
+            CorrectOption = correctOption;
+        }
     }
 }
