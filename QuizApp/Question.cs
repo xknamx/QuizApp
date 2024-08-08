2718ec55d4f38be3cf213b4deb4bccac20805e0b
ChiyunaZen <uni202406_11@telework.to>
2024-08-08 11:17:02 +0900

    Questionクラスをcsファイルに分割

---
+++ QuizApp/Question.cs
@@ -0,0 +1,20 @@
+﻿namespace QuizApp
+{
+    //クイズ文、選択支リスト、回答番号の設定を保持するQuestionクラス
+    public class Question
+    {
+        public string Text { get; set; }
+        public string[] Options { get; set; }
+        public int CorrectOption { get; set; }
+
+        public Question(string text, string[] options, int correctOption)
+        {
+            Text = text;
+            Options = options;
+            CorrectOption = correctOption;
+        }
+        
+
+    }
+
+}
